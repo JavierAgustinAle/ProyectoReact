65599455ff9e36d51f18237799cffc77aeaa98fc
JavierAle <javierale25@live.com>
2020-05-14 20:26:04 -0300

    Usando el State del componente y el setState vamos mostrando dinamicamente los valores que ingresa el user en los input del formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,34 +1,59 @@
 import React, { Component } from "react";
+import '../assets/css/styles.scss';
 
 
 class Formulario extends Component {
 
     constructor(props) {  //Creo una instancia de la clase Formulario. Hago esto cuando recibe PROPS
         super(props)
+        
+        this.state = {
+            nombre: "",
+            correo: ""
+        }
     }
 
     render() {
         return(
             <React.Fragment>
                 <div className="ed-grid">
-                    <h1>Soy un formulario</h1>
+                    <h1>Formulario de Inscripcion</h1>
                     <form>
-                            <div className="form__item">
-                                <label htmlFor="nombre"> Nombre Completo</label>
-                                <input type="text" name="nombre"></input>
-                            </div>
-                            <div className="form__item">
-                                <label htmlFor="email"> Correo Electronico</label>
-                                <input type="email" name="email"></input>
-                            </div>
-                            <div className="form__item">
-                                <input className="button full" type="submit" value="ENVIAR" />
-                            </div>
-                     </form>
+                        <div className="ed-grid m-grid-2">
+                            
+                                    <div className="form__item">
+                                        <label htmlFor="nombre"> Nombre Completo</label>
+                                        <input 
+                                            type="text" 
+                                            name="nombre" 
+                                            onChange = { e => this.setState({  //Cuando cambie le asigo al state nombre el valor de e
+                                                nombre: e.target.value
+                                            })}
+                                        />
+                                    </div>
+                                    <div className="form__item">
+                                        <label htmlFor="email"> Correo Electronico</label>
+                                        <input 
+                                            type="email" 
+                                            name="email" 
+                                            onChange = {e => this.setState({
+                                                correo: e.target.value
+                                            })}
+                                        />
+                                    </div>
+                            
+                        </div>
+                    </form>
+                    <h2>{`Hola ${this.state.nombre}`}</h2>
+                    <span>{`Tu email es: ${this.state.correo}`}</span>
                 </div>
+                
             </React.Fragment>
         );
     }
 }
 
 export default Formulario;
+
+//Usando el onChange y dentro un callback para llamar al setState, puedo cambiar el valor de nombre o correo de forma dinamica,
+    //asignandole el valor que va ingresando el usuario en los input
